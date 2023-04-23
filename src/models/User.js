de006ca06dc39a2faff655d77e215fbc81404da9
Thim7 <93660448+Thim7@users.noreply.github.com>
2023-04-23 22:31:14 +0700

    Update DB User and User Controller

--- src/models/User.js
+++ src/models/User.js
@@ -2,10 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const User = new Schema ({
-    name: { type: String, minLength: 1, maxLength: 100 },
+    isManager: {type: Boolean, default:false},
+    userName: { type: String, minLength: 1, maxLength: 100 },
+    passWord: { type: String, minLength: 1, maxLength: 100 },
+    fullName: { type: String, minLength: 1, maxLength: 100 },
+    image: { type: String, minLength: 1, maxLength: 100 },
+    gender: { type: String, minLength: 1, maxLength: 4 },
+    email: { type: String, minLength: 1, maxLength: 100 },
     address: { type: String, minLength: 1, maxLength: 255 },
     city: { type: String, minLength: 1, maxLength: 100 },
-    phone: { type: String, minLength: 1, maxLength: 14 },
+    phoneNumber: { type: String, minLength: 1, maxLength: 14 },
+    birth: { type: Date},
     order: { type: mongoose.Schema.Types.ObjectId, ref: "Order"}
 })
 
