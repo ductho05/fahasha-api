234187ffcba358d40b0c54602b80ff53c6013374
ducanh1811 <119037853+ducanh1811@users.noreply.github.com>
2023-04-24 20:27:02 +0700

    update 24/04

---
+++ src/routes/category.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const categoryController = require("../controllers/CategoryControllers");
+
+router.get("/", categoryController.getAllCategory);
+router.get("/:id", categoryController.getCategoryById);
+
+module.exports = router;
