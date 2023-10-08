3705e7e89cce2de5af8dd02832a5cbf9f6b073d8
Phan Văn Đức Anh <93977845+aducanha@users.noreply.github.com>
2023-10-08 15:30:12 +0700

    khong upload thu muc constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,9 +7,9 @@ class Constants {
  urlapi ="https://bookstore-api-0a2i.onrender.com/bookstore/api/v1"
     
 // local
- urlui = "http://localhost:3456"
+ // urlui = "http://localhost:3456"
 // deloy
- //urlui = "https://fahasha-clone.onrender.com"
+urlui = "https://fahasha-clone.onrender.com"
    
 }
 
