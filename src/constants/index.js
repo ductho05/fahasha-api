b51f3499c47a9355e6836bd99e9d39465498aba9
Phan Văn Đức Anh <93977845+aducanha@users.noreply.github.com>
2023-10-08 12:52:16 +0700

    khong upload thu muc constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,16 @@
 class Constants {
     TOKEN_KEY = 'ductho2002';
+    
+// local
+//const urlapi = "http://localhost:3000/bookstore/api/v1"
+// deloy
+ urlapi ="https://bookstore-api-0a2i.onrender.com/bookstore/api/v1"
+    
+// local
+//const urlui = "http://localhost:3456"
+// deloy
+ urlui = "https://fahasha-clone.onrender.com"
+   
 }
 
 module.exports = new Constants
