10625bc0a152272d46091e0792f865d8b21e2b52
Đức Thọ <104706778+ductho05@users.noreply.github.com>
2023-11-26 13:21:50 +0700

    Update webpush.js

--- src/config/webpush.js
+++ src/config/webpush.js
@@ -3,9 +3,12 @@ require('dotenv').config()
 
 function configWebPush() {
 
+    const publicKey = Buffer.from(process.env.PUBLIC_KEY, 'base64')
+    const privateKey = Buffer.from(process.env.PRIVATE_KEY, 'base64')
+
     const vapiKeys = {
-        publicKey: process.env.PUBLIC_KEY,
-        privateKey: process.env.PRIVATE_KEY
+        publicKey: publicKey,
+        privateKey: privateKey
     }
     webpush.setVapidDetails('mailto:bookstore.ta.group@gmail.com', vapiKeys.publicKey, vapiKeys.privateKey)
 }
